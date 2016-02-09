e5ba0faa78b2c6afaa49054bab50e5edabbdf44d
cav71 <a.cavallo@cavallinux.eu>
2016-02-09 20:20:43 +0000

    updates

--- osc/plugins.py
+++ osc/plugins.py
@@ -6,13 +6,32 @@
 from __future__ import print_function
 
 import os
+import sys
+import path
 import imp
 import inspect
+import contextlib
+import zipimport
+import zipfile
+
+
+try:
+    import jinja2
+except ImportError:
+    jinja2 = None
 
 
 def is_compiled(py_filename):
     with open(py_filename, 'rb') as fp:
         return fp.read(len(imp.get_magic())) == imp.get_magic()
+
+
+@contextlib.contextmanager
+def setpath(wdir):
+    osys = sys.path[:]
+    sys.path.insert(0, wdir)        
+    yield
+    sys.path = osys
 
 
 def load_from_source(py_filename, modname=None, inject=None, include_modules=True):
@@ -39,3 +58,40 @@ def load_from_source(py_filename, modname=None, inject=None, include_modules=Tru
             result[name] = data
 
     return result
+
+
+def load_from_dir(py_dir, modname=None, inject=None, include_modules=True, main='main.py'):
+    modname = modname or os.path.basename(py_dir)
+    py_filename = os.path.join(py_dir, main)
+    inject = inject or {}
+
+    # adds support for jinja templating in a plugin
+    if jinja2 and not 'jenv' in inject:
+        loader = jinja2.FileSystemLoader([ os.path.join(py_dir, 'templates'), ])
+        # We can add more to the jinja env..
+        inject['jenv'] = jinja2.Environment(loader=loader)
+
+    with setpath(py_dir):
+        return load_from_source(py_filename, modname=modname, 
+                    inject=inject, include_modules=include_modules)
+
+
+def load_from_zipfile(zip_filename, modname=None, inject=None, include_modules=True, main='main.py'):
+
+    root, ext = os.path.splitext(zip_filename)
+    modname = modname or os.path.basename(root)
+
+    with setpath(zip_filename):
+        zfp = zipimport.zipimporter(zip_filename)
+        #with zipfile.ZipFile(zip_filename) as zfp:
+        #    zfp.printdir()
+        #    print(zfp.read('plugin.with.jinja/' + main))
+        #print(zfp.get_code('main'))
+    
+
+    #py_filename = os.path.join(py_dir, main)
+    #if jinja2:
+    #    inject = inject or {}
+    #return load_from_source(py_filename, modname=modname, inject=inject, include_modules=include_modules)
+
+
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -12,7 +12,7 @@ def suite():
     return unittest.makeSuite(TestPackageStatus)
 
 
-class TestModule(unittest.TestCase):
+class TestFromSource(unittest.TestCase):
     def test_load_from_source(self):
         filename = os.path.join(FIXTURES_DIR, 'simple.py')
 
@@ -35,9 +35,67 @@ class TestModule(unittest.TestCase):
             [ 'hello', 'os', 'path', ])
 
 # simple4
-        found = osc.plugins.load_from_source(filename, modname='simple4', inject={ 'unittest' : unittest })
+        found = osc.plugins.load_from_source(filename, modname='simple4', 
+                    inject={ 'unittest' : unittest })
         self.assertEquals(sorted(found.keys()), 
             [ 'hello', 'os', 'path', 'unittest', ])
+
+
+class TestFromDir(unittest.TestCase):
+    def test_load_fromdir(self):
+        py_dir = os.path.join(FIXTURES_DIR, 'inadir')
+
+# simple5
+        found = osc.plugins.load_from_dir(py_dir, modname='simple5')
+        self.assertEquals(sorted(found.keys()), 
+            [ 'hello2', 'os', 'path', 'wow', ])
+        
+# simple6
+        found = osc.plugins.load_from_dir(py_dir, modname='simple6', 
+                inject={ 'unittest' : unittest })
+        self.assertEquals(sorted(found.keys()), 
+            [ 'hello2', 'os', 'path', 'unittest', 'wow', ])
+
+    def test_load_fromdir_with_jinja(self):
+
+        py_dir = os.path.join(FIXTURES_DIR, 'plugin.with.jinja')
+
+# simple7
+        found = osc.plugins.load_from_dir(py_dir, modname='simple7')
+        self.assertEquals(sorted(found.keys()), 
+            [ 'test0', 'test1', 'test2', ])
+        self.assertMultiLineEqual(found['test0']('World'),'''
+Hello from the base
+
+Simple example, you passed name=World
+''')
+
+        self.assertMultiLineEqual(found['test1']('World'),'''
+Hello from the base
+
+
+This block from base
+
+Simple example, you passed name=World
+''')
+
+        self.assertMultiLineEqual(found['test2']('World'),'''
+Hello from the base
+
+
+This block from base
+
+This block from example where you gave the name=World
+'''.lstrip())
+
+
+class TestFromZip(unittest.TestCase):
+    def test_from_zip(self):
+        zip_filename = os.path.join(FIXTURES_DIR, 'plugin.with.jinja.zip')
+
+# simple8
+        found = osc.plugins.load_from_zipfile(zip_filename, modname='simple8')
+        print found
 
 
 if __name__ == '__main__':
